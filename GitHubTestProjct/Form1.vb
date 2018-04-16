42d7cc17a660dcb9bc7336702cc7d0f753cb5bbc
HayasakaTakehiro <t.hayasaka@yti.co.jp>
2018-04-16 10:57:15 +0900

    third commit

--- GitHubTestProjct/Form1.vb
+++ GitHubTestProjct/Form1.vb
@@ -1,5 +1,9 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim i As Integer = 0
+        Dim total As Integer = 0
+
+        total = i + i
 
     End Sub
 End Class
--- JMANAGE.NET/Jupiter_net/Jupiter_net/Application.xaml.vb
+++
@@ -1,6 +0,0 @@
-﻿Class Application
-
-    ' Startup、Exit、DispatcherUnhandledException などのアプリケーション レベルのイベントは、
-    ' このファイルで処理できます。
-
-End Class
