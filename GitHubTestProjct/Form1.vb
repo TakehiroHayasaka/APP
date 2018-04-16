42cd9292175679901139543f97bc52cbaea8679c
HayasakaTakehiro <t.hayasaka@yti.co.jp>
2018-04-16 10:37:29 +0900

    Revert "secound commit"
    This reverts commit 87cb41b2ed9e47e7898ba9c90858ade166404033.

--- GitHubTestProjct/Form1.vb
+++ GitHubTestProjct/Form1.vb
@@ -1,12 +1,5 @@
 ﻿Public Class Form1
-    Private _count As Integer = 0
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-
-        _count += 1
 
     End Sub
 End Class
