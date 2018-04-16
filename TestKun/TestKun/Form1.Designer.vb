a6bace3848d7c64184b7440e643c8386f776f406
HayasakaTakehiro <t.hayasaka@yti.co.jp>
2018-04-16 15:25:24 +0900

    add anything

--- TestKun/TestKun/Form1.Designer.vb
+++ TestKun/TestKun/Form1.Designer.vb
@@ -23,6 +23,7 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Button1
@@ -35,19 +36,31 @@ Partial Class Form1
         Me.Button1.Text = "へぇ～"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(200, 274)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(75, 24)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 24.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(484, 461)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
         Me.Font = New System.Drawing.Font("MS UI Gothic", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
         Me.Margin = New System.Windows.Forms.Padding(7, 6, 7, 6)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
 End Class
--- TestKun/TestKun/Form1.vb
+++ TestKun/TestKun/Form1.vb
@@ -1,5 +1,22 @@
 ﻿Public Class Form1
+    Private _cnt As Integer = 0
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        If _cnt = 100 Then
+            Return
+        End If
+
+        _cnt += 1
+        Me.Label1.Text = _cnt.ToString
+        Me.Refresh()
+
+    End Sub
+
+    Private Sub ShowClearMessage()
 
     End Sub
 End Class
